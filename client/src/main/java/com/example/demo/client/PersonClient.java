e38afdad6c151a9011dcd792bda11e05d7db098b
Humberto Dias <hldtux@gmail.com>
2023-04-06 00:25:42 -0300

    Removing cache

--- client/src/main/java/com/example/demo/client/PersonClient.java
+++ client/src/main/java/com/example/demo/client/PersonClient.java
@@ -1,8 +1,6 @@
 package com.example.demo.client;
 
 import com.example.demo.dto.PersonDto;
-import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,11 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @FeignClient(value = "person-client", url = "${person.client.host}", path = "/api")
-@CacheConfig(cacheResolver = "customCacheResolver")
 public interface PersonClient {
 
     @GetMapping(value = "faker")
-    @Cacheable(cacheNames = "person", key = "{#amount, #delay, #cacheManager}")
     List<PersonDto> getPerson(@RequestParam(value = "amount", defaultValue = "10", required = false) int amount, @RequestParam(value = "delay", defaultValue = "0", required = false) int delay, String cacheManager);
 
 }
